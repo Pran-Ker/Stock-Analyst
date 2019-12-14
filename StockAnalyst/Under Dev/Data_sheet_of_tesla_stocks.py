133c53ecb2d1098bb040d2824c5fa80bba688e5f
Prannay Hebbar <52480636+Pran-Ker@users.noreply.github.com>
2019-12-15 02:03:14 +0530

    Update Data_sheet_of_tesla_stocks.py

--- StockAnalyst/Under Dev/Data_sheet_of_tesla_stocks.py
+++ StockAnalyst/Under Dev/Data_sheet_of_tesla_stocks.py
@@ -1,14 +1,32 @@
-
 from datetime import datetime as dt
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from matplotlib import style
 import pandas as pd
 import pandas_datareader.data as web
 
+#Plot the
 style.use('ggplot')
 
-start=dt(2016,1,1)
-end= dt(2016,12,31)
+###Setting the Time Frame *IMP make sure it is recent
 
-df= web.DataReader('TSLA','yahoo',start,end)
-print(df.head(6))
+#start=dt(2016,1,1)
+#end= dt(2016,12,31)
+
+### the scraps yahoo (or any other similar website for stock sheets) for the desired stock
+#df= web.DataReader('TSLA','yahoo',start,end)
+
+#print(df.head(6))
+#df.to_csv('tsla.csv')
+
+
+
+### Read the built .csv file
+df = pd.read_csv('tsla.csv', parse_dates=True)
+
+
+### Display the Stocks Sheet
+#print(df.head(10))
+
+
+df.plot()
+plt.show()
